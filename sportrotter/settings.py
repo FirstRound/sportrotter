776febf3fd6de913df31512dcd33d9173a8fdcc5
bigblackbug <bigblackbugg@gmail.com>
2017-07-30 00:46:14 +0300

    fixed migrations removed address app

--- sportrotter/settings.py
+++ sportrotter/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'activities.apps.ActivitiesConfig',
-    'address',
+    # 'address',
     'rest_framework.authtoken',
     'django_filters',
     'authentication',
